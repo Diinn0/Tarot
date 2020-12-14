69e4bcc62081729ab28db5c08a7812810555b51a
Maël Roy <dinocraftdidi@gmail.com>
2020-12-14 10:13:50 +0100

    Add Utils class
    This class contain usefull method

---
+++ src/fr/mael/tarot/utils/Utils.java
@@ -0,0 +1,44 @@
+package fr.mael.tarot.utils;
+
+import java.io.File;
+
+public class Utils {
+	
+    public final static String jpeg = "jpeg";
+    public final static String jpg = "jpg";
+    public final static String gif = "gif";
+    public final static String tiff = "tiff";
+    public final static String tif = "tif";
+    public final static String png = "png";
+    public final static String json = "json";
+
+    /*
+     * Get the extension of a file.
+     */  
+    public static String getExtension(File f) {
+        String ext = null;
+        String s = f.getName();
+        int i = s.lastIndexOf('.');
+
+        if (i > 0 &&  i < s.length() - 1) {
+            ext = s.substring(i+1).toLowerCase();
+        }
+        return ext;
+    }
+
+	public static String integerToRoman(int num) {
+        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] romanLiterals = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+        StringBuilder roman = new StringBuilder();
+
+        for(int i=0;i<values.length;i++) {
+            while(num >= values[i]) {
+                num -= values[i];
+                roman.append(romanLiterals[i]);
+            }
+        }
+        return roman.toString();
+    }
+	
+}
